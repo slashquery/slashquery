a2eeeea5adba24d6c8e48ea054f9bf52af39ce8f
nbari <nbari@tequila.io>
2017-06-12 17:10:08 +0200

    new file:   proxy_test.go

---
+++ proxy_test.go
@@ -0,0 +1,71 @@
+package slashquery
+
+import (
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/nbari/violetear"
+	"github.com/slashquery/resolver"
+)
+
+func TestProxy(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("slashquery /?"))
+	}))
+	defer ts.Close()
+
+	r, err := resolver.New("4.2.2.2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	routes := make(map[string]*Route)
+	routes["test"] = &Route{
+		URL: ts.URL,
+	}
+	sq := &Slashquery{
+		Config:    make(map[string]string),
+		Resolver:  r,
+		Routes:    routes,
+		Servers:   make(map[string]*Servers),
+		Upstreams: make(map[string]*Upstream),
+	}
+	if err := sq.Setup(); err != nil {
+		t.Error(err)
+	}
+	sq.ResolveUpstreams()
+	testRoute := sq.Routes["test"]
+	expect(t, testRoute.URL, ts.URL)
+	u, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Error(err)
+	}
+	testUpstream, ok := sq.Upstreams[u.Host]
+	if !ok {
+		t.Errorf("Expecting upstream: %s", testUpstream)
+	}
+	testServers, ok := sq.Servers[u.Host]
+	if !ok {
+		t.Errorf("Expecting servers: %s", testServers)
+	}
+	host, _, err := net.SplitHostPort(u.Host)
+	if err != nil {
+		t.Error(err)
+	}
+	expect(t, testServers.Addresses[0], host)
+
+	// test router
+	router := violetear.New()
+	router.Verbose = false
+	router.LogRequests = false
+	router.Handle("/*", sq.Proxy("test"), "GET,HEAD")
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	router.ServeHTTP(w, req)
+
+	expect(t, w.Body.String(), "slashquery /?")
+}
